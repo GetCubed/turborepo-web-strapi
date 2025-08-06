1012d825a2bde34892f70422dc98518eda3c48d1
ro <rhodri.mativo@gmail.com>
2025-08-05 22:52:21 -0400

    ✨ feat(hero): add hero page outline and include in tailwind config

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,27 +1,14 @@
+import { Hero } from "@/components/dynamic-zone/hero";
+
 export default function HomePage(): JSX.Element {
   return (
-    <main className="outline flex flex-col justify-center items-center h-screen">
-      <div className="outline flex flex-row gap-4">
-        <h1>Hello, World!</h1>
-
-        <button className="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
-          Button
-        </button>
-
-        <button className="btn btn-primary">Button</button>
-        <button className="btn">Button</button>
-      </div>
-
-      <div className="outline flex flex-col gap-4">
-        <h1>Hello, World!</h1>
-
-        <button className="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
-          Button
-        </button>
-
-        <button className="btn btn-primary">Button</button>
-        <button className="btn">Button</button>
-      </div>
+    <main className="border-4 border-white flex flex-col justify-center items-center h-screen w-full">
+      <Hero
+        heading="Hello, World!"
+        sub_heading="Welcome to our website"
+        CTAs={["Get Started", "Learn More"]}
+        locale="en"
+      />
     </main>
   );
 }
--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -4,7 +4,7 @@ import daisyui from "daisyui";
 const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    // "./components/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
     // "./pages/**/*.{js,ts,jsx,tsx}",
     // "./src/**/*.{js,ts,jsx,tsx}",
   ],
