210d20e771988adfc8dd0727f8cc348e172e2915
ro <rhodri.mativo@gmail.com>
2025-08-07 23:09:17 -0400

    ✨ feat: add shooting background, shooting stars, and fade-in animation

--- apps/web/app/components/dynamic-zone/hero.tsx
+++ apps/web/app/components/dynamic-zone/hero.tsx
@@ -1,9 +1,23 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
 
 import { Heading } from "../elements/heading";
 import { Button } from "../elements/button";
 import { Subheading } from "../elements/subheading";
+
+import { motion } from "motion/react";
+
+import ShootingStars from "../decorations/shooting-star";
+import StarBackground from "../decorations/star-background";
+
+type CTA = {
+  id: string | number;
+  URL: string;
+  text: string;
+  variant?: string;
+};
 
 export const Hero = ({
   heading,
@@ -13,11 +27,20 @@ export const Hero = ({
 }: {
   heading: string;
   sub_heading: string;
-  CTAs: any[];
+  CTAs: CTA[];
   // locale: string;
 }) => {
   return (
     <div className="h-screen flex flex-col items-center justify-center">
+      {/* fade in shooting stars and star background */}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.5 }}
+      >
+        <StarBackground />
+        <ShootingStars />
+      </motion.div>
       <Heading
         as="h1"
         className="text-4xl md:text-4xl lg:text-8xl font-semibold max-w-7xl mx-auto text-center mt-6 relative z-10 py-6"
@@ -31,17 +54,26 @@ export const Hero = ({
       </Subheading>
       <div className="flex space-x-2 items-center mt-8">
         {CTAs &&
-          CTAs.map((cta) => (
-            <Button
-              key={cta?.id}
-              as={Link}
-              href={`/${cta.URL}`}
-              {...(cta.variant && { variant: cta.variant })}
-            >
-              {cta.text}
-            </Button>
-          ))}
+          CTAs.map((cta) => {
+            return (
+              <Button
+                key={cta?.id}
+                as={Link}
+                href={`/`}
+                {...(cta.variant && {
+                  variant: cta.variant as
+                    | "outline"
+                    | "simple"
+                    | "primary"
+                    | "muted",
+                })}
+              >
+                {cta.text}
+              </Button>
+            );
+          })}
       </div>
+      <div className="absolute inset-x-0 bottom-0 h-80 w-full bg-gradient-to-t from-charcoal to-transparent" />
     </div>
   );
 };
